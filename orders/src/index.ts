74a6aa7f38fbcf5697f1c12cd133dfcd64ab4544
Erick Escobar <ericksap@yahoo.co.uk>
2020-10-18 19:26:39 -0400

    commit 10

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -11,7 +11,7 @@ import { PaymentCreatedListener } from './events/listeners/listener-payment-crea
 
 
 const start = async () => {
-    console.log('Starting app - training');
+    console.log('Starting app - training NEW');
 
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
