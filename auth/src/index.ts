d4fba37db5808dd06ebf6d01ec63d2be766b6352
Erick Escobar <ericksap@yahoo.co.uk>
2020-10-18 18:16:54 -0400

    commit 4

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { app }  from "./app";
 import {DatabaseConnectionError} from '@bpx-training/common';
 
 const start = async () => {
+    console.log('Starting app');
+    
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
